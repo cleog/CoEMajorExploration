411a4d2228e64fa6415efb829d4b861947520e13
Lawrence Kim <kimla@oregonstate.edu>
2023-11-16 19:53:28 +0900

    Added MongoDB sample code and connection to DB

---
+++ MajorExplNextJS/server/index.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb+srv://coe_admin:CoE123%232023!@cs46x.l19wpnk.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    const database = client.db('CoE');
+    const Students = database.collection('Students');
+
+    const query = { role: 'first year' };
+    const firstyear = await Students.findOne(query);
+    console.log(firstyear);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
